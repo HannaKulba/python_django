f17555cd8c25f3003df400f5f730e34c11d29245
HannaKulba <anna.mirraza@gmail.com>
2020-03-24 10:58:21 +0300

    add app mysite

---
+++ mysite/polls/tests.py
@@ -0,0 +1,13 @@
+import requests
+
+
+def test_login_to_django_dashboard():
+    url = 'http://127.0.0.1:8000/admin/login/'
+    response_0 = requests.get(url)
+    csrftoken = response_0.cookies['csrftoken']
+    username = 'admin'
+    password = 'admin2020'
+    login_data = {'username': username, 'password': password, 'csrfmiddlewaretoken': csrftoken, 'next': '/admin/'}
+    response = requests.post(url, data=login_data, cookies=response_0.cookies)
+    assert response.status_code == 200
+    assert response.url == 'http://127.0.0.1:8000/admin/'
