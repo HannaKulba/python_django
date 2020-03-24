02ac2519f57e55036c0eaeb7b41dcb5ca731858b
HannaKulba <anna.mirraza@gmail.com>
2020-03-24 11:20:32 +0300

    update test

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -9,5 +9,6 @@ def test_login_to_django_dashboard():
     password = 'admin2020'
     login_data = {'username': username, 'password': password, 'csrfmiddlewaretoken': csrftoken, 'next': '/admin/'}
     response = requests.post(url, data=login_data, cookies=response_0.cookies)
+    print(response.text)
     assert response.status_code == 200
     assert response.url == 'http://127.0.0.1:8000/admin/'
